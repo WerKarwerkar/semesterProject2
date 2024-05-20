0e99cee8759efaa43708f415e596b7f0818e1e9f
WerKarwerkar <126430261+WerKarwerkar@users.noreply.github.com>
2024-05-20 11:47:12 +0200

    profile and avatar

--- src/js/index.js
+++ src/js/index.js
@@ -30,3 +30,69 @@ document.addEventListener("DOMContentLoaded", function() {
         console.error("An error occurred while checking the login state:", error);
     });
 });
+
+async function changeAvatar(){
+    const avatarUrl = document.getElementById('avatar').value;
+
+    const accessToken = localStorage.getItem('accesToken')
+     const name = (JSON.parse(localStorage.getItem("profile")).name)
+
+
+    const data = {
+        bio: "",
+        avatar: {
+            url: `${avatarUrl}`,
+            alt: "User avatar"
+        },
+        banner: {
+            url: `${avatarUrl}`,
+            alt: "User baner"
+        }
+    };
+    try{
+
+        const url = `https://v2.api.noroff.dev/auction/profiles/${name}`;
+        const options = {
+            method: 'PUT',
+            headers: {
+                'Authorization': `Bearer ${accessToken}`,
+                "X-Noroff-API-Key": "ab56ab33-bc31-4630-a6a2-4c6744dbe4ab",
+                'Content-Type': 'application/json' 
+            },
+            body: JSON.stringify(data)
+        };
+        
+        const fetchResponse = await fetch(url, options);
+        const jsondata = await fetchResponse.json();
+        const profile = jsondata.data
+        // console.log(fetchResponse, name)
+        if(fetchResponse.ok){
+            ShowMessage(false, `Avatar changed`, "")
+        }else{
+            ShowMessage(true, `Cannot find URL`, "")
+        }
+        
+    }catch(error){
+        
+    }
+
+}
+
+function ShowMessage(error, message, location){
+    const reg_message_display = document.querySelector('#avatar-error');
+    var reg_message = ""
+    if(error){
+        // console.error(message);
+        reg_message_display.innerHTML = message
+    }else{
+        // console.log(message);
+       
+            reg_message_display.innerHTML = message
+            setTimeout(() => {
+                window.location.reload()
+            }, 1000);
+        
+    }
+}
+
+
---
+++ src/js/profile/getProfile.mjs
@@ -0,0 +1,40 @@
+import { API_PROFILE } from "../constants.mjs";
+import { getStorage } from "../storage.js";
+
+const accessToken = getStorage('accesToken')
+const name = (JSON.parse(getStorage("profile")).name)
+
+window.getProfile = async function(){
+    const url = API_PROFILE+`${name}`;
+    const options = {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${accessToken}`,
+            "X-Noroff-API-Key": "ab56ab33-bc31-4630-a6a2-4c6744dbe4ab",
+            'Content-Type': 'application/json' 
+        },
+      
+    };
+    
+    const fetchResponse = await fetch(url, options);
+    const jsondata = await fetchResponse.json();
+    const profile = jsondata.data
+
+    let html = `
+    <div class="row">
+        <div class="col d-flex justify-content-evenly align-items-center px-5">
+            <img id="avatar-display" src="${profile.avatar.url}" class="img-fluid" alt="${profile.avatar.alt}">
+            <h4 class="user"> Hello - ${profile.name}</h4>
+        </div>
+    </div>
+    <input type="text" id="avatar">
+    <button onclick="changeAvatar()">Zmien</button>
+    <h4 class="" id="avatar-error"></h4>
+      <hr class="hr-custom mt-5">
+    `
+
+    document.querySelector('#profile-display').innerHTML = html
+    document.querySelector('#credits').innerHTML = ` ${profile.credits} Credits`
+    // window.location.reload()
+}
+getProfile()
